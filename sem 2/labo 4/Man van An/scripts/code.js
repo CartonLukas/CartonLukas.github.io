789dbded006a1084f39cb1719df6aefccdbb22e8
CartonLukas <lukas.carton2@student.vives.be>
2023-03-12 09:52:24 +0100

    labo 4

--- sem 2/labo 4/Man van An/scripts/code.js
+++ sem 2/labo 4/Man van An/scripts/code.js
@@ -5,20 +5,20 @@ const setup = () => {
 };
 
 const telVoorkomens = () => {
-    let txtTekst=document.getElementById("txtTekst");
-    let tekst=txtTekst.value;
-    console.log(tekst);
+    let tekst=document.getElementById("txtTekst").value;
 
-    let txtZoekTekst=document.getElementById("txtZoekTekst");
-    let zoekTekst=txtZoekTekst.value;
-    console.log(zoekTekst);
+
+
+    let zoekTekst=document.getElementById("txtZoekTekst").value;
+
+
     let result=0;
     let idx=tekst.indexOf(zoekTekst);
-    console.log(idx);
+
     while (idx!==-1) {
         result++;
         idx=tekst.indexOf(zoekTekst, idx+zoekTekst.length);
-        console.log(idx);
+
     }
     let txtAantal=document.getElementById("txtAantal");
 
